c5bf6d5b6f7097f56d97c51d943478febe6dedf8
BlackBear <111135227+BllackBear@users.noreply.github.com>
2023-05-18 04:01:28 +0300

    Create config.lua

---
+++ bb_repair/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+-- Repair location coordinates
+Config.RepairLocation = vector3(100.0, 200.0, 300.0) -- Replace with your desired coordinates
+
+-- Distance threshold for repair location check
+Config.RepairDistanceThreshold = 10.0
+
+-- Admin ACE permission required to use the command anywhere
+Config.AdminPermission = 'admin'
