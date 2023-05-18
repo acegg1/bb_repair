f657caf0a075f5d389faf115d08aec6a0f54c8a9
BlackBear <111135227+BllackBear@users.noreply.github.com>
2023-05-18 04:02:05 +0300

    Update config.lua

--- bb_repair/config.lua
+++ bb_repair/config.lua
@@ -1,7 +1,10 @@
 Config = {}
 
 -- Repair location coordinates
-Config.RepairLocation = vector3(100.0, 200.0, 300.0) -- Replace with your desired coordinates
+Config.RepairLocations = {
+    vector3(100.0, 200.0, 300.0), -- Replace with your desired coordinates
+    vector3(400.0, 500.0, 600.0), -- Add more repair locations as needed
+}
 
 -- Distance threshold for repair location check
 Config.RepairDistanceThreshold = 10.0
